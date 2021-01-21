48267d8f30584b7255d8f96f8874ff4f5d106bf4
moretrim <49937701+moretrim@users.noreply.github.com>
2021-01-21 18:25:55 +0100

    Use more consistent terminology

--- source/Types.hs
+++ source/Types.hs
@@ -76,6 +76,41 @@ instance Monoid Identifier where
 -- Localisation --
 ------------------
 
+{-
+A point of terminology. The typical CSV localisation file looks like the following:
+
+    ### This localisation file has been automatically generated ###;English;French;German;Polish;Spanish;Italian;Swedish;Czech;Hungarian;Dutch;Portuguese;Russian;Finnish;
+    MILITARY_PERSONALITY;Personality: §Y$NAME$§W;Personnalité : §Y$NAME$§W;Persönlichkeit: §Y$NAME$§W;;Personalidad: §Y$NAME$§W;;;;;;;;;
+
+(Simplified to a CSV header followed by another line.)
+
+First, some game quirks:
+
+- these “CSV” files really involve semicolon separators (which is not too unusual, to be fair)
+- using number signs ‘#’ to introduce comments of sorts is a convention—this is expanded upon in the
+  following
+- the header of sorts on the first line is also a convention
+- very little quoting is supported by the game, all quotation marks are taken verbatim
+- instead there is a little bit of escaping, e.g. a literal “\n” will be interpreted as a newline
+- these are not technically text files because in this pre-Unicode era of PDS games they liked to
+  mix encodings, which can be a true headache—see `Localisation` for more details
+- there are more interesting behaviours, but they are outside the scope of this program
+
+The anatomy of a line is as follows:
+
+    KEY;TRANSLATION;TRANSLATION;TRANSLATION;…
+
+- we call a single a line a row as well as a (localisation) entry
+- the first row item is the (localisation) key: it corresponds to an in-game or in-script item which
+  can be subject to quoting there, but never in the localisation as per the quirks
+- consequently pseudo-comment keys of the kind “## meant to be a comment;;;…” are actually reachable
+  from script, but it has to be deliberate so it’s fine
+- each item subsequent to the key is one of its translations (one of the purposes of our nice
+  headers is to remind the reader which translation is which)
+- anything beyond the last item is crud, of which the game is fairly tolerant (see `Localisation`)
+
+-}
+
 -- | Localisation key. Not to be confused with script-side identifiers, They are always unquoted and
 -- raw.
 type Key = Text
@@ -86,9 +121,8 @@ type OrderedKeys = NonEmpty Key
 -- | Not everything is translated into every language, especially with mods.
 type Translation = Maybe Text
 
--- | The translation entries expected of this era of PDS games. We have to be lenient because
--- malformed localisation files are the norm rather than the exception, and the game itself accepts
--- them.
+-- | The translations expected of this era of PDS games. We have to be lenient because malformed
+-- localisation files are the norm rather than the exception, and the game itself accepts them.
 type Translations =
     ( Translation -- ^ English
     , Translation -- ^ French
@@ -171,7 +205,7 @@ each'' _ =
 -- | Localisation entry: a key with its associated translations.
 type Entry = (Key, Translations)
 
--- | Localisation data: keys that map to translations. Note that the keys are stored unquoted.
+-- | Localisation data: keys that map to translations.
 type Localisation = HashMap Text Translations
 
 -- | Ordered localisation data.
